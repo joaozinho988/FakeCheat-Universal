d529437b34e735abfa67fa2bbbea89221c1c7917
joaozinho988 <95830680+joaozinho988@users.noreply.github.com>
2025-11-23 23:42:07 -0300

    Implement utility functions in utils.lua
    Added utility functions for notifications and random positioning.

--- utils.lua
+++ utils.lua
@@ -1,1 +1,18 @@
+-- utils.lua - funções auxiliares universais
 
+local utils = {}
+
+function utils:Notify(msg, color)
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Fake Cheat",
+        Text = msg,
+        Duration = 3
+    })
+end
+
+function utils:RandomPos()
+    return UDim2.new(0, math.random(80, 900), 0, math.random(80, 500))
+end
+
+return utils
