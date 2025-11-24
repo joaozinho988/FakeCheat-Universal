f1ccd052c5a7819be50ca07dc01273581fa4725c
joaozinho988 <95830680+joaozinho988@users.noreply.github.com>
2025-11-23 23:42:54 -0300

    Add silent aim functionality with text display
    Implement fake silent aim feature with text notifications.

--- silent.lua
+++ silent.lua
@@ -1,1 +1,31 @@
+-- silent.lua - fake silent aim (textos aparecendo)
 
+local utils = require(script.Parent.utils)
+
+local silent = {}
+silent.Enabled = false
+
+local gui = game.Players.LocalPlayer.PlayerGui
+
+function silent:Toggle()
+    self.Enabled = not self.Enabled
+
+    if not self.Enabled then return end
+
+    task.spawn(function()
+        while self.Enabled do
+            local txt = Instance.new("TextLabel", gui)
+            txt.Text = "silent hit!"
+            txt.TextColor3 = Color3.fromRGB(255, 255, 0)
+            txt.BackgroundTransparency = 1
+            txt.Size = UDim2.new(0, 140, 0, 30)
+            txt.Position = utils:RandomPos()
+            txt.TextScaled = true
+
+            task.delay(0.4, function() txt:Destroy() end)
+            task.wait(0.07)
+        end
+    end)
+end
+
+return silent
